ebd10ce2285942ea329bb8466e75515e2215419c
Marcel AL NG <marcel.nguyen@gmail.com>
2019-01-08 13:36:38 +0100

    update static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
 root 'application#hello'
+	get 'static_pages/home'
+	get 'static_pages/help'
+	
 	get '/signup', to: 'users#new'
 	get '/login', to: 'session#new'
 	post '/login', to: 'sessions#create'
