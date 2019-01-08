577fd52c0d3e43518d13cea779fb9f834c91cfff
Marcel AL NG <marcel.nguyen@gmail.com>
2019-01-08 13:22:14 +0100

    1st day follow one more time railstutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+	def hello
+		render html: "hello,world tuto from  https://www.railstutorial.org/book/beginning"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'application#hello'
+	get '/signup', to: 'users#new'
+	get '/login', to: 'session#new'
+	post '/login', to: 'sessions#create'
+	delete '/logou', to: 'sessions#destroy'
+resources :users  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
